8d92373c0ee8fe80fced20326bcd2853818d89a3
Justin Willis <jgw9617@gmail.com>
2016-08-19 14:42:15 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function magic() {
+    // test for indexeddb
+    if (window.indexedDB) {
+        // wkwebview
+        alert ('You are on ios 10 pwa in safari, cordova etc');
+        if (window.applePaySession === undefined) {
+            alert ('You are on a pwa added to the homescreen');
+        }
+    } else {
+        // no wkwebview
+        alert ('You are on ios 9 cordova or ios 8 anything');
+    }
+}

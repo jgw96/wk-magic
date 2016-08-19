375d3f24e0a9391fce8fd1ac2e78ff49e654a555
Justin Willis <jgw9617@gmail.com>
2016-08-19 15:05:14 -0500

    derpedy derp

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 function magic() {
     // test for indexeddb
-    if (window.indexedDB) {
+    if (window.indexedDB && window.ApplePaySession) {
         // wkwebview
-        alert ('You are on ios 10: pwa in safari, cordova etc');
-        if (window.ApplePaySession === undefined) {
-            alert ('You are on a pwa added to homescreen')
-        }
-    } else {
+        alert('You are on wkwebview in cordova, safari, ios 10');
+    } else if (window.indexedDB && window.ApplePaySession === undefined) {
+        // broken wkwebview
+        alert('You are on a pwa on ios 10 added to homescreen with broken scroll')
+    }
+    else {
         // no wkwebview
-        alert ('You are on ios 9 cordova or ios 8 anything');
+        alert('You are on ios 9 cordova or ios 8 anything');
     }
 }
 

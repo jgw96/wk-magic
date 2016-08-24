9a6e058fb5a83a43b4fba25f053a6307cdf1034f
Justin Willis <jgw9617@gmail.com>
2016-08-24 14:41:06 -0500

    extra check

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function magic() {
     if (window.indexedDB) {
         // wkwebview
         alert('You are on wkwebview in cordova, safari, ios 10 or in another browser besides safari');
-    } else if (window.navigator.standalone) {
+    } else if (window.indexedDB && window.navigator.standalone) {
         // broken wkwebview
         alert('You are on a pwa on ios 10 added to homescreen with broken scroll')
     }

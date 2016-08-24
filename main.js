0a1cf5eb7b494a4543a61d1457ff0eff73b47b8b
Justin Willis <jgw9617@gmail.com>
2016-08-24 14:39:50 -0500

    fix

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function magic() {
     // test for indexeddb
-    if (window.indexedDB && window.ApplePaySession) {
+    if (window.indexedDB) {
         // wkwebview
-        alert('You are on wkwebview in cordova, safari, ios 10');
-    } else if (window.indexedDB && window.ApplePaySession === undefined) {
+        alert('You are on wkwebview in cordova, safari, ios 10 or in another browser besides safari');
+    } else if (window.navigator.standalone) {
         // broken wkwebview
         alert('You are on a pwa on ios 10 added to homescreen with broken scroll')
     }
